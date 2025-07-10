133fcf7e6b1b48eb51ebe189df31b476406e2e63
pengi-blog <tanishparida3@gmail.com>
2025-07-10 05:51:19 +0000

    Deploying to gh-pages from @ pengi-blog/pengi-blog.github.io@d8d46357f8a1f89828db3924430921a774d1bd2d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,17 +24,6 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/first-post/";
-          
-        },
-      },{id: "post-first-post",
-        
-          title: "first post",
-        
-        description: "first post!",
-        section: "Posts",
-        handler: () => {
-          
             window.location.href = "/blog/2025/summer-first-post/";
           
         },
